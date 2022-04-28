18e6687084145e67abf5f88d79d9e3014ae9be69
Vinicius <viniholtman28@gmail.com>
2022-04-28 13:39:21 -0300

    Verify curtomer already exists

--- src/index.js
+++ src/index.js
@@ -9,12 +9,17 @@ const customers = [];
 
 app.post("/account", (req, res) => {
   const { name, cpf } = req.body;
-  const id = uuidv4();
+  
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Customer already exists!"})
+  }
 
   customers.push({
     name,
     cpf,
-    id,
+    id: uuidv4(),
     statement: []
   })
 
