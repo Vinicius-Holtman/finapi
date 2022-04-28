9e28bf9fb1844f150a544a52d1d492935bc07a86
Vinicius <viniholtman28@gmail.com>
2022-04-28 15:56:47 -0300

    Created middleware for customer

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ app.use(express.json())
 
 const customers = [];
 
+// Middleware
+function verifyExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found!"})
+  }
+
+  req.customer = customer;
+
+  return next(); 
+}
+
 app.post("/account", (req, res) => {
   const { name, cpf } = req.body;
   
@@ -26,14 +41,8 @@ app.post("/account", (req, res) => {
   return res.status(201).send()
 })
 
-app.get("/statement", (req, res) => {
-  const { cpf } = req.headers;
-
-  const customer = customers.find(customer => customer.cpf === cpf)
-
-  if (!customer) {
-    return res.status(400).json({ error: "Customer not found!"})
-  }
+app.get("/statement", verifyExistsAccountCPF, (req, res) => {
+  const { customer } = req;
 
   return res.json(customer.statement)
 })
