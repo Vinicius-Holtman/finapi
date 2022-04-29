bdd050387b6f884c5836fa1e2d7686cb81cdb2f2
vinicius <viniholtman28@gmail.com>
2022-04-28 22:54:18 -0300

    Created route for get statement by date

--- src/index.js
+++ src/index.js
@@ -97,4 +97,17 @@ app.post("/withdraw", verifyExistsAccountCPF, (req, res) => {
   return res.status(201).send()
 })
 
+app.get("/statement/date", verifyExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  const { date } = req.query
+
+  const dateFormat = new Date(date + " 00:00")
+
+  const statement = customer.statement.filter((statement) => {
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString()
+  })
+
+  return res.json(statement)
+})
+
 app.listen(3000, () => console.log("Server is running!! 🥸"))
